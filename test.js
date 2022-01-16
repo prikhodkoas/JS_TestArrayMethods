18486f06957219dc99063b5502f2a8c9324a35a9
Alex <49212602+prikhodkoas@users.noreply.github.com>
2022-01-16 09:05:33 +0300

    uncoment

--- test.js
+++ test.js
@@ -15,9 +15,9 @@ let people = [
 ////// Вывод всего массива объектов через цикл for
 //////############################################
 
-// for(let i = 0; i < people.length; i++){
-//     console.log(people[i]);
-// }
+for(let i = 0; i < people.length; i++){
+    console.log(people[i]);
+}
 
 //////############################################
 
@@ -28,9 +28,9 @@ let people = [
 ////// Вывод всего массива объектов через цикл for_of
 //////###############################################
 
-// for(let element of people){
-//     console.log(element);
-// }
+for(let element of people){
+    console.log(element);
+}
 
 //////###############################################
 
@@ -42,18 +42,18 @@ let people = [
 //////#########################################################
 
 //// 1.1. Полная запись
-// people.forEach(function(element, index, array){
-//     console.log(element);
-// })
+people.forEach(function(element, index, array){
+    console.log(element);
+})
 
 //// 1.2. Тоже самое, только вместо функции, стрелочная функция
-// people.forEach((element, index, array) => console.log(element));
+people.forEach((element, index, array) => console.log(element));
 
 //// 1.3. Тоже самое, только уберем не используемые аргументы
-// people.forEach(element => console.log(element));
+people.forEach(element => console.log(element));
 
 //// 1.4. Тоже самое, только сократим название до р (Как в шарпах :))) )
-// people.forEach(p => console.log(p));
+people.forEach(p => console.log(p));
 
 //////#########################################################
 
@@ -65,44 +65,44 @@ let people = [
 //#############################################################
 
 //// 1.1. Делаем такой же массив (но он какой-то пронумерованный)
-// const newPeople = people.map(function(element, index, array){
-//     return element;
-// });
-// console.log(newPeople);
+const newPeople = people.map(function(element, index, array){
+    return element;
+});
+console.log(newPeople);
 
 //// 1.2. Тоже самое, только вместо функции, стрелочная функция
-// const newPeople = people.map((element, index, array) => element);
-// console.log(newPeople);
+const newPeople = people.map((element, index, array) => element);
+console.log(newPeople);
 
 //// 1.3. Тоже самое, только уберем не используемые аргументы
-// const newPeople = people.map(element => element);
-// console.log(newPeople);
+const newPeople = people.map(element => element);
+console.log(newPeople);
 
 //// 1.4. Тоже самое, только сократим название до р (Как в шарпах :))) )
-// const newPeople = people.map(p => p);
-// console.log(newPeople);
+const newPeople = people.map(p => p);
+console.log(newPeople);
 
 //// 2.1. Делаем новый массив только с именами
-// const namesPeople = people.map(function(elem, ind, arr){
-//     return elem.name;
-// });
-// console.log(namesPeople);
+const namesPeople = people.map(function(elem, ind, arr){
+    return elem.name;
+});
+console.log(namesPeople);
 
 //// 2.2. Тоже самое, но совсем коротко
-// const namesPeople = people.map(p => p.name);
-// console.log(namesPeople);
+const namesPeople = people.map(p => p.name);
+console.log(namesPeople);
 
 //// 3.1. Делаем новый массив с именем и возрастом людей (без бюджета)
-// const newPeople = people.map((elem, ind, arr) => {
-//     return {name: elem.name, age: elem.age}
-// });
-// console.log(newPeople);
+const newPeople = people.map((elem, ind, arr) => {
+    return {name: elem.name, age: elem.age}
+});
+console.log(newPeople);
 
 //// 3.2. Тоже самое, но совсем коротко
 //// Доп. скобки нужно ставить что бы фигурные скобки объекта
 //// не принимались за тело функции
-// const newPeople = people.map(p => ({name: p.name, age: p.age}));
-// console.log(newPeople);
+const newPeople = people.map(p => ({name: p.name, age: p.age}));
+console.log(newPeople);
 
 //#############################################################
 
@@ -114,30 +114,30 @@ let people = [
 //#############################################################
 
 //// 1.1. Отбираем в новый массив, людей старше 18 лет сначала for
-// const newPeople = [];
-// for(let i = 0; i < people.length; i++){
-//     if(people[i].age > 18){
-//         newPeople.push(people[i]);
-//     }
-// }
-// console.log(newPeople);
+const newPeople = [];
+for(let i = 0; i < people.length; i++){
+    if(people[i].age > 18){
+        newPeople.push(people[i]);
+    }
+}
+console.log(newPeople);
 
 //// 1.2. Теперь тоже самое с помощью filter
-// const newPeople = people.filter((elem, ind, arr) => {
-//     if(elem.age > 18){
-//         return true;
-//     }
-// });
-// console.log(newPeople);
+const newPeople = people.filter((elem, ind, arr) => {
+    if(elem.age > 18){
+        return true;
+    }
+});
+console.log(newPeople);
 
 //// 1.2. Теперь сокращаем. (убираем не используемые параметры)
 //// и учитывая, что возвращать нужно bool, то убираем if
-// const newPeople = people.filter(elem => elem.age > 18);
-// console.log(newPeople);
+const newPeople = people.filter(elem => elem.age > 18);
+console.log(newPeople);
 
 //// 1.3. Тоже самое, но совсем коротко
-// const newPeople = people.filter(p => p.age > 18);
-// console.log(newPeople);
+const newPeople = people.filter(p => p.age > 18);
+console.log(newPeople);
 
 //#############################################################
 
@@ -149,40 +149,40 @@ let people = [
 //#############################################################
 
 //// 1.1. Сначала через for
-// let total = 0;
-// for(let i = 0; i < people.length; i++)
-// {
-//     total += people[i].budget;
-// }
-// console.log(total);
+let total = 0;
+for(let i = 0; i < people.length; i++)
+{
+    total += people[i].budget;
+}
+console.log(total);
 
 //// 1.2. Теперь с помощью reduce
 //// reduce принимает два параметра 1-функция, 2-начальное значение
 //// в функции два параметра 1-callBack переменная 2-переменная текущей итерации
-// const total = people.reduce((res, element) => {
-//     return res + element.budget;
-// }, 0);
-// console.log(total);
+const total = people.reduce((res, element) => {
+    return res + element.budget;
+}, 0);
+console.log(total);
 
 //// 1.3. Теперь сокращаем
-// const total = people.reduce((res, p) => res + p.budget, 0)
-// console.log(total);
+const total = people.reduce((res, p) => res + p.budget, 0)
+console.log(total);
 
 //// 2.1. Считаем средний возраст людей (сначала через for)
-// let average = 0;
-// for(let i = 0; i < people.length; i++){
-//     average += people[i].age;
-// }
-// average = Math.round(average / people.length);
-// console.log(average);
+let average = 0;
+for(let i = 0; i < people.length; i++){
+    average += people[i].age;
+}
+average = Math.round(average / people.length);
+console.log(average);
 
 //// 2.2. Теперь используем в вычислениях reduce
-// const average = Math.round(people.reduce((age, person) => age + person.age, 0) / people.length);
-// console.log(average);
+const average = Math.round(people.reduce((age, person) => age + person.age, 0) / people.length);
+console.log(average);
 
 //// 2.3. Укоротим совсем немножко (только имена переменных)
-// const average = Math.round(people.reduce((res, p) => res + p.age, 0) / people.length);
-// console.log(average);
+const average = Math.round(people.reduce((res, p) => res + p.age, 0) / people.length);
+console.log(average);
 
 //#############################################################
 
@@ -194,8 +194,8 @@ let people = [
 //#############################################################
 
 //// 1.1. Сразу ищем с помощью find
-// const person = people.find(p => p.age > 5 && p.age < 20);
-// console.log(person);
+const person = people.find(p => p.age > 5 && p.age < 20);
+console.log(person);
 
 //#############################################################
 
